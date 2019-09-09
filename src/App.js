a18b29c3175d3cfa6612b7e9767bbf6f0f39a6a7
Davor Ergotic <dergotic@pnimedia.com>
2019-09-09 11:52:56 -0700

    Fibonacci: Problem solution

--- src/App.js
+++ src/App.js
@@ -1,26 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./fibonacci.jpeg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Fibonacci extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "", members: [] };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.calculateFibonacci = this.calculateFibonacci.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.calculateFibonacci(this.state.value);
+  }
+
+  calculateFibonacci(input) {
+    let inputConverted = Number(input);
+
+    if (
+      Number.isNaN(inputConverted) ||
+      !Number.isInteger(inputConverted) ||
+      inputConverted === 0
+    ) {
+      alert("Please enter valid number (whole), you entered: " + input);
+      this.setState({ value: "", members: [] });
+      return;
+    }
+
+    if (inputConverted < 2) {
+      this.setState({ members: [0, 1] });
+      return [0, 1];
+    } else {
+      let sequence = this.calculateFibonacci(inputConverted - 1);
+      sequence.push(
+        sequence[sequence.length - 1] + sequence[sequence.length - 2]
+      );
+      this.setState({ members: sequence });
+      return sequence;
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Fibonacci sequence members.</p>
+        </header>
+
+        <section className="App-section">
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Enter the number of Fibonacci members you want to calulate: <br />
+              <input
+                type="text"
+                value={this.state.value}
+                onChange={this.handleChange}
+                title="Please enter a whole number value."git 
+              />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+
+          {this.state.members.map((member, index) => (
+            <p className="Fibonacci-members" key={index}>
+              {member}
+              {index < this.state.members.length - 1 ? ",\u00A0" : ""}
+            </p>
+          ))}
+        </section>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Fibonacci;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,67 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import Fibonacci from "./App";
+import { shallow } from "enzyme";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test("Renders component without crashing", () => {
+  shallow(<Fibonacci />);
+});
+
+describe("Fibonacci sequence tests", () => {
+  it("Should return 0, 1 when user enters 1 as an input", () => {
+    const wrapper = shallow(<Fibonacci />);
+    const instance = wrapper.instance();
+
+    expect(wrapper.state("members")).toStrictEqual([]);
+    instance.calculateFibonacci(1);
+    expect(wrapper.state("members")).toStrictEqual([0, 1]);
+  });
+
+  it("Should return 0, 1, 1, 2 when user enters 3 as an input", () => {
+    const wrapper = shallow(<Fibonacci />);
+    const instance = wrapper.instance();
+
+    expect(wrapper.state("members")).toStrictEqual([]);
+    instance.calculateFibonacci(3);
+    expect(wrapper.state("members")).toStrictEqual([0, 1, 1, 2]);
+  });
+
+  it("Should return nothing when input is invalid (not a number)", () => {
+    const wrapper = shallow(<Fibonacci />);
+    const instance = wrapper.instance();
+    window.alert = () => {};
+
+    expect(wrapper.state("members")).toStrictEqual([]);
+    instance.calculateFibonacci("string");
+    expect(wrapper.state("members")).toStrictEqual([]);
+  });
+
+  it("Should return nothing when input is invalid (empty string/no input)", () => {
+    const wrapper = shallow(<Fibonacci />);
+    const instance = wrapper.instance();
+    window.alert = () => {};
+
+    expect(wrapper.state("members")).toStrictEqual([]);
+    instance.calculateFibonacci("");
+    expect(wrapper.state("members")).toStrictEqual([]);
+  });
+
+  it("should return nothing when input is invalid (not an integer input)", () => {
+    const wrapper = shallow(<Fibonacci />);
+    const instance = wrapper.instance();
+    window.alert = () => {};
+
+    expect(wrapper.state("members")).toStrictEqual([]);
+    instance.calculateFibonacci("11.123");
+    expect(wrapper.state("members")).toStrictEqual([]);
+  });
+
+  it("should return nothing when input is invalid (0 as input)", () => {
+    const wrapper = shallow(<Fibonacci />);
+    const instance = wrapper.instance();
+    window.alert = () => {};
+
+    expect(wrapper.state("members")).toStrictEqual([]);
+    instance.calculateFibonacci(0);
+    expect(wrapper.state("members")).toStrictEqual([]);
+  });
 });
